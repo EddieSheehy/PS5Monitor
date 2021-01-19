316ddf9d8d6719b484815b9ffcb69e36dad5e170
EddieSheehy <51293802+EddieSheehy@users.noreply.github.com>
2021-01-19 21:38:34 +0000

    Update PS5Disk.py

--- PS5Disk.py
+++ PS5Disk.py
@@ -8,12 +8,12 @@ TOKEN = "NzExMjU2NjU4NTkyMTM3MjM3.XsAXYQ.RsuGF9pIAtU3dguVz7-EclQRy34"
 
 client = discord.Client()
 webhookurl = 'https://discord.com/api/webhooks/800840213135228948/DpuqgulYFHYHuQhgy7g-NVj78E6H9kE6dgvQHdzHmX5EWzWVh45ck0AJT5RIupjYgeIr'
-imgURL = 'https://static-ie.gamestop.ie/images/products/275145/3max.jpg'
+imgURL = 'https://static-ie.gamestop.ie/images/products/271916/3max.jpg'
 
 #Webhook Structure for Smyths IE
 def webhookresponsesmyths():
              webhook = DiscordWebhook(url=webhookurl, username="Smyths IE", avatar_url='https://pbs.twimg.com/profile_images/1201814930581868544/f0N7G3DI_400x400.png')
-             embed = DiscordEmbed(title='PS5 Digital In Stock', url='https://www.smythstoys.com/ie/en-ie/video-games-and-tablets/playstation-5/playstation-5-consoles/playstation-5-digital-edition-console/p/191430', color=15105570)
+             embed = DiscordEmbed(title='PS5 Disk In Stock', url='https://www.smythstoys.com/ie/en-ie/video-games-and-tablets/playstation-5/playstation-5-consoles/playstation-5-digital-edition-console/p/191430', color=15105570)
              embed.add_embed_field(name='Type', value='Restock', inline=True)
              embed.add_embed_field(name='Site', value='Smyths IE', inline=True)
              embed.set_footer(text='Watson',icon_url='https://cdn.discordapp.com/app-icons/711256658592137237/74a1779046799c1665d03cda5bb9694f.png')
@@ -25,7 +25,7 @@ def webhookresponsesmyths():
 #Webhook Structure for GameStop IE
 def webhookresponsegamestop():
              webhook = DiscordWebhook(url=webhookurl, username="Gamestop IE", avatar_url='https://jobapplications.net/wp-content/uploads/gamestop-logo-icon.png')
-             embed = DiscordEmbed(title='PS5 Digital In Stock', url='https://www.gamestop.ie/PlayStation%205/Games/74863/playstation-5-digital-edition-console', color=7419530)
+             embed = DiscordEmbed(title='PS5 Disk In Stock', url='https://www.gamestop.ie/PlayStation%205/Games/74863/playstation-5-digital-edition-console', color=7419530)
              embed.add_embed_field(name='Type', value='Restock', inline=True)
              embed.add_embed_field(name='Site', value='Gamestop IE', inline=True)
              embed.set_footer(text='Watson',icon_url='https://cdn.discordapp.com/app-icons/711256658592137237/74a1779046799c1665d03cda5bb9694f.png')
@@ -46,12 +46,14 @@ async def on_ready():
         content = requests.get(urlGS)
         soup = bs.BeautifulSoup(content.text, 'lxml')
         stockGS = soup.find("div", {"class": "bigBuyButtons SPNOpenMap"}).find('a').text
+        pass
 
         #Smyths
         urlSmyths = 'https://www.smythstoys.com/ie/en-ie/video-games-and-tablets/playstation-5/playstation-5-consoles/playstation-5-console/p/191259'
         content = requests.get(urlSmyths)
         soup = bs.BeautifulSoup(content.text, 'lxml')
         stockSmyths = str(soup.find("form", {"id": "customAddToCartForm"}).find('button'))
+        pass
         if stockGS == 'Out Of Stock':
             print('Gamestop Disk Out Of Stock\n')
 
