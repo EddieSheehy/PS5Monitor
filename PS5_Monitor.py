65c635e16e25644f17e7ec9f3bd02eaa30bb2ce3
EddieSheehy <51293802+EddieSheehy@users.noreply.github.com>
2021-01-23 03:09:11 +0000

    Update PS5_Monitor.py

--- PS5_Monitor.py
+++ PS5_Monitor.py
@@ -15,7 +15,7 @@ imgURLDig = 'https://static-ie.gamestop.ie/images/products/275145/3max.jpg'
 #Webhook Structure for Smyths IE
 def webhookresponsesmythsDisk():
              webhook = DiscordWebhook(url=webhookurl, username="Smyths IE", avatar_url='https://pbs.twimg.com/profile_images/1201814930581868544/f0N7G3DI_400x400.png')
-             embed = DiscordEmbed(title='PS5 Disk In Stock', url='https://www.smythstoys.com/ie/en-ie/video-games-and-tablets/playstation-5/playstation-5-consoles/playstation-5-digital-edition-console/p/191430', color=15105570)
+             embed = DiscordEmbed(title='PS5 Disk In Stock', url='https://www.smythstoys.com/ie/en-ie/video-games-and-tablets/playstation-5/playstation-5-consoles/playstation-5-console/p/191259', color=15105570)
              embed.add_embed_field(name='Type', value='Restock', inline=True)
              embed.add_embed_field(name='Site', value='Smyths IE', inline=True)
              embed.set_footer(text='Watson',icon_url='https://cdn.discordapp.com/app-icons/711256658592137237/74a1779046799c1665d03cda5bb9694f.png')
@@ -27,7 +27,7 @@ def webhookresponsesmythsDisk():
 #Webhook Structure for GameStop IE
 def webhookresponsegamestopDisk():
              webhook = DiscordWebhook(url=webhookurl, username="Gamestop IE", avatar_url='https://jobapplications.net/wp-content/uploads/gamestop-logo-icon.png')
-             embed = DiscordEmbed(title='PS5 Disk In Stock', url='https://www.gamestop.ie/PlayStation%205/Games/74863/playstation-5-digital-edition-console', color=7419530)
+             embed = DiscordEmbed(title='PS5 Disk In Stock', url='https://www.gamestop.ie/PlayStation%205/Games/72504/playstation-5-console', color=7419530)
              embed.add_embed_field(name='Type', value='Restock', inline=True)
              embed.add_embed_field(name='Site', value='Gamestop IE', inline=True)
              embed.set_footer(text='Watson',icon_url='https://cdn.discordapp.com/app-icons/711256658592137237/74a1779046799c1665d03cda5bb9694f.png')
